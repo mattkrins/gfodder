bc79c5eff4d8eeeafe90ae8e3d5025b406f8bfa2
Matt Krins <mattkrins@gmail.com>
2017-07-31 20:01:58 +1000

    Hotfix

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,5 +1,5 @@
 GM.Name = "gfodder"
-DEVELOPER_MODE=true
+
 if DEVELOPER_MODE then DeriveGamemode( "sandbox" ) print(GM.Name..": Initialized in Developer Mode.") else DeriveGamemode( "base" ) print(GM.Name..": Initialized in StandAlone Mode.") end
 
 team.SetUp( 1, "Loading", Color( 255, 40, 40 ) )
